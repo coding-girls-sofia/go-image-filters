85e703d16a2331fa2427a36c105d96d5b1162088
Evgeni Kunev <evgeni@uber.com>
2018-10-07 21:01:37 +0300

    Reading the file and printing the image type

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"log"
 	"os"
+
+	_ "image/jpeg"
+	_ "image/png"
 )
 
 func main() {
 	filePath := os.Args[1]
 	fmt.Printf("Attempting to read image from %s\n", filePath)
+	imageFile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, format, err := image.Decode(imageFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Read a %s image \n", format)
 	return
 }
